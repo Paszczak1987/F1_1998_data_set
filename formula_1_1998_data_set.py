6689de15024c8262d448545d7222d048bd7f6906
Łukasz Paszko <47369744+Paszczak1987@users.noreply.github.com>
2024-10-10 22:30:15 +0200

    Add files via upload

---
+++ formula_1_1998_data_set.py
@@ -0,0 +1,412 @@
+f1_1998_season = {
+    'scoring': {
+        '1st': 10,
+        '2nd': 6,
+        '3rd': 4,
+        '4th': 3,
+        '5th': 2,
+        '6th': 1
+    },
+    'teams': {
+        'williams': {
+            'car': 'FW20',
+            'nationality': 'Great Britain',
+            'drivers': [
+                {
+                    'name': 'Damon Hill',
+                    'nationality': 'Great Britain'
+                },
+                {
+                    'name': 'Jacques Villeneuve',
+                    'nationality': 'Canada'
+                }
+            ],
+        },
+        'ferrari': {
+            'car': 'F300',
+            'nationality': 'Italy',
+            'drivers': [
+                {
+                    'name': 'Michael Schumacher',
+                    'nationality': 'Germany'
+                },
+                {
+                    'name': 'Eddie Irvine',
+                    'nationality': 'Great Britain'
+                }
+            ],
+        },
+        'benetton': {
+            'car': 'B198',
+            'nationality': 'Italy',
+            'drivers': [
+                {
+                    'name': 'Giancarlo Fisichella',
+                    'nationality': 'Italy'
+                },
+                {
+                    'name': 'Alexander Wurz',
+                    'nationality': 'Austria'
+                }
+            ],
+        },
+        'mclaren': {
+            'car': 'MP4/13',
+            'nationality': 'Great Britain',
+            'drivers': [
+                {
+                    'name': 'David Coulthard',
+                    'nationality': 'Great Britain'
+                },
+                {
+                    'name': 'Mika Häkkinen',
+                    'nationality': 'Finland'
+                }
+            ],
+        },
+        'jordan': {
+            'car': '198',
+            'nationality': 'Ireland',
+            'drivers': [
+                {
+                    'name': 'Heinz-Harald Frentzen',
+                    'nationality': 'Germany'
+                },
+                {
+                    'name': 'Ralf Schumacher',
+                    'nationality': 'Germany'
+                }
+            ],
+        },
+        'peugeot': {
+            'car': 'AP01',
+            'nationality': 'France',
+            'drivers': [
+                {
+                    'name': 'Olivier Panis',
+                    'nationality': 'France'
+                },
+                {
+                    'name': 'Jarno Trulli',
+                    'nationality': 'Italy'
+                }
+            ],
+        },
+        'sauber': {
+            'car': 'C17',
+            'nationality': 'Switzerland',
+            'drivers': [
+                {
+                    'name': 'Jean Alesi',
+                    'nationality': 'France'
+                },
+                {
+                    'name': 'Johnny Herbert',
+                    'nationality': 'Great Britain'
+                }
+            ],
+        },
+        'arrows': {
+            'car': 'A19',
+            'nationality': 'Great Britain',
+            'drivers': [
+                {
+                    'name': 'Pedro Diniz',
+                    'nationality': 'Brazil'
+                },
+                {
+                    'name': 'Mika Salo',
+                    'nationality': 'Finland'
+                }
+            ],
+        },
+        'stewart': {
+            'car': 'SF02',
+            'nationality': 'Great Britain',
+            'drivers': [
+                {
+                    'name': 'Rubens Barrichello',
+                    'nationality': 'Brazil'
+                },
+                {
+                    'name': 'Jan Magnussen',
+                    'nationality': 'Denmark'
+                },
+                {
+                    'name': 'Jos Verstappen',
+                    'nationality': 'Netherlands'
+                }
+            ],
+        },
+        'tyrrell': {
+            'car': '026',
+            'nationality': 'Great Britain',
+            'drivers': [
+                {
+                    'name': 'Ricardo Rosset',
+                    'nationality': 'Brazil'
+                },
+                {
+                    'name': 'Toranosuke Takagi',
+                    'nationality': 'Japan'
+                }
+            ],
+        },
+        'minardi': {
+            'car': 'M198',
+            'nationality': 'Italy',
+            'drivers': [
+                {
+                    'name': 'Pedro Diniz',
+                    'nationality': 'Brazil'
+                },
+                {
+                    'name': 'Esteban Tuero',
+                    'nationality': 'Argentina'
+                }
+            ],
+        },
+    },
+    'rounds': {
+        'australia': {
+            'summary': {
+                'winner': 'Mika Häkkinen',
+                'pole_position': 'Mika Häkkinen',
+                'fastest_lap': 'Mika Häkkinen'
+            },
+            'race_result': {
+                '1st': 'Mika Häkkinen',
+                '2nd': 'David Coulthard',
+                '3rd': 'Heinz-Harald Frentzen',
+                '4th': 'Eddie Irvine',
+                '5th': 'Jacques Villeneuve',
+                '6th': 'Johnny Herbert',
+            },
+        },
+        'brazil': {
+            'summary': {
+                'winner': 'Mika Häkkinen',
+                'pole_position': 'Mika Häkkinen',
+                'fastest_lap': 'Mika Häkkinen'
+            },
+            'race_result': {
+                '1st': 'Mika Häkkinen',
+                '2nd': 'David Coulthard',
+                '3rd': 'Michael Schumacher',
+                '4th': 'Alexander Wurz',
+                '5th': 'Heinz-Harald Frentzen',
+                '6th': 'Giancarlo Fisichella',
+            },
+        },
+        'argentina': {
+            'summary': {
+                'winner': 'Michael Schumacher',
+                'pole_position': 'David Coulthard',
+                'fastest_lap': 'Alexander Wurz'
+            },
+            'race_result': {
+                '1st': 'Michael Schumacher',
+                '2nd': 'Mika Häkkinen',
+                '3rd': 'Eddie Irvine',
+                '4th': 'Alexander Wurz',
+                '5th': 'Jean Alesi',
+                '6th': 'David Coulthard',
+            },
+        },
+        'san_marino': {
+            'summary': {
+                'winner': 'David Coulthard',
+                'pole_position': 'David Coulthard',
+                'fastest_lap': 'Michael Schumacher'
+            },
+            'race_result': {
+                '1st': 'David Coulthard',
+                '2nd': 'Michael Schumacher',
+                '3rd': 'Eddie Irvine',
+                '4th': 'Jacques Villeneuve',
+                '5th': 'Heinz-Harald Frentzen',
+                '6th': 'Jean Alesi',
+            },
+        },
+        'spain': {
+            'summary': {
+                'winner': 'Mika Häkkinen',
+                'pole_position': 'Mika Häkkinen',
+                'fastest_lap': 'Mika Häkkinen'
+            },
+            'race_result': {
+                '1st': 'Mika Häkkinen',
+                '2nd': 'David Coulthard',
+                '3rd': 'Michael Schumacher',
+                '4th': 'Alexander Wurz',
+                '5th': 'Rubens Barrichello',
+                '6th': 'Jacques Villeneuve',
+            },
+        },
+        'monaco': {
+            'summary': {
+                'winner': 'Mika Häkkinen',
+                'pole_position': 'Mika Häkkinen',
+                'fastest_lap': 'Mika Häkkinen'
+            },
+            'race_result': {
+                '1st': 'Mika Häkkinen',
+                '2nd': 'Giancarlo Fisichella',
+                '3rd': 'Eddie Irvine',
+                '4th': 'Mika Salo',
+                '5th': 'Jacques Villeneuve',
+                '6th': 'Pedro Diniz',
+            },
+        },
+        'canada': {
+            'summary': {
+                'winner': 'Michael Schumacher',
+                'pole_position': 'David Coulthard',
+                'fastest_lap': 'Michael Schumacher'
+            },
+            'race_result': {
+                '1st': 'Michael Schumacher',
+                '2nd': 'Giancarlo Fisichella',
+                '3rd': 'Eddie Irvine',
+                '4th': 'Alexander Wurz',
+                '5th': 'Rubens Barrichello',
+                '6th': 'Jan Magnussen',
+            },
+        },
+        'france': {
+            'summary': {
+                'winner': 'Michael Schumacher',
+                'pole_position': 'Mika Häkkinen',
+                'fastest_lap': 'David Coulthard'
+            },
+            'race_result': {
+                '1st': 'Michael Schumacher',
+                '2nd': 'Eddie Irvine',
+                '3rd': 'Mika Häkkinen',
+                '4th': 'Jacques Villeneuve',
+                '5th': 'Alexander Wurz',
+                '6th': 'David Coulthard',
+            },
+        },
+        'britain': {
+            'summary': {
+                'winner': 'Michael Schumacher',
+                'pole_position': 'Mika Häkkinen',
+                'fastest_lap': 'Michael Schumacher'
+            },
+            'race_result': {
+                '1st': 'Michael Schumacher',
+                '2nd': 'Mika Häkkinen',
+                '3rd': 'Eddie Irvine',
+                '4th': 'Alexander Wurz',
+                '5th': 'Giancarlo Fisichella',
+                '6th': 'Ralf Schumacher',
+            },
+        },
+        'austria': {
+            'summary': {
+                'winner': 'Mika Häkkinen',
+                'pole_position': 'Mika Häkkinen',
+                'fastest_lap': 'David Coulthard'
+            },
+            'race_result': {
+                '1st': 'Mika Häkkinen',
+                '2nd': 'David Coulthard',
+                '3rd': 'Michael Schumacher',
+                '4th': 'Eddie Irvine',
+                '5th': 'Ralf Schumacher',
+                '6th': 'Jacques Villeneuve',
+            },
+        },
+        'german': {
+            'summary': {
+                'winner': 'Mika Häkkinen',
+                'pole_position': 'Mika Häkkinen',
+                'fastest_lap': 'David Coulthard'
+            },
+            'race_result': {
+                '1st': 'Mika Häkkinen',
+                '2nd': 'David Coulthard',
+                '3rd': 'Jacques Villeneuve',
+                '4th': 'Damon Hill',
+                '5th': 'Michael Schumacher',
+                '6th': 'Ralf Schumacher',
+            },
+        },
+        'hungary': {
+            'summary': {
+                'winner': 'Michael Schumacher',
+                'pole_position': 'Mika Häkkinen',
+                'fastest_lap': 'Michael Schumacher'
+            },
+            'race_result': {
+                '1st': 'Michael Schumacher',
+                '2nd': 'David Coulthard',
+                '3rd': 'Jacques Villeneuve',
+                '4th': 'Damon Hill',
+                '5th': 'Heinz-Harald Frentzen',
+                '6th': 'Mika Häkkinen',
+            },
+        },
+        'belgium': {
+            'summary': {
+                'winner': 'Damon Hill',
+                'pole_position': 'Mika Häkkinen',
+                'fastest_lap': 'Michael Schumacher'
+            },
+            'race_result': {
+                '1st': 'Damon Hill',
+                '2nd': 'Ralf Schumacher',
+                '3rd': 'Jean Alesi',
+                '4th': 'Heinz-Harald Frentzen',
+                '5th': 'Pedro Diniz',
+                '6th': 'Jarno Trulli',
+            },
+        },
+        'italy': {
+            'summary': {
+                'winner': 'Michael Schumacher',
+                'pole_position': 'Michael Schumacher',
+                'fastest_lap': 'Mika Häkkinen'
+            },
+            'race_result': {
+                '1st': 'Michael Schumacher',
+                '2nd': 'Eddie Irvine',
+                '3rd': 'Ralf Schumacher',
+                '4th': 'Mika Häkkinen',
+                '5th': 'Jean Alesi',
+                '6th': 'Damon Hill',
+            },
+        },
+        'luxembourg': {
+            'summary': {
+                'winner': 'Mika Häkkinen',
+                'pole_position': 'Michael Schumacher',
+                'fastest_lap': 'Mika Häkkinen'
+            },
+            'race_result': {
+                '1st': 'Mika Häkkinen',
+                '2nd': 'Michael Schumacher',
+                '3rd': 'David Coulthard',
+                '4th': 'Eddie Irvine',
+                '5th': 'Heinz-Harald Frentzen',
+                '6th': 'Giancarlo Fisichella',
+            },
+        },
+        'japan': {
+            'summary': {
+                'winner': 'Mika Häkkinen',
+                'pole_position': 'Michael Schumacher',
+                'fastest_lap': 'Michael Schumacher'
+            },
+            'race_result': {
+                '1st': 'Mika Häkkinen',
+                '2nd': 'Eddie Irvine',
+                '3rd': 'David Coulthard',
+                '4th': 'Damon Hill',
+                '5th': 'Heinz-Harald Frentzen',
+                '6th': 'Jacques Villeneuve',
+            },
+        },
+    }
+}
